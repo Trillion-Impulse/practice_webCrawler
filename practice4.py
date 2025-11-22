1e35a10ea09324668f3e067ee441c19e441323a4
Trillion_Impulse <tkdduq2897@naver.com>
2025-11-22 16:51:10 +0900

    feat(practice4.py): AJAX 역공학 크롤링 시도 (실패 기록)

---
+++ practice4.py
@@ -0,0 +1,56 @@
+import requests
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+
+# 검색어
+query = "삼성전자"
+# 요청 URL
+url = "https://s.search.naver.com/p/newssearch/3/api/tab/more"
+
+# Devtools에서 확인한 Query String Parameters
+params = {
+    "query": "query",  # 검색어
+    "start": 1,          # 시작 뉴스 번호
+    "sort": 0,            # 정렬 방식
+    "nso": "so:r,p:all,a:all"  # 기간/뉴스 타입 등
+}
+
+# Referer에 query를 포함할 경우 반드시 URL 인코딩
+referer = f"https://search.naver.com/search.naver?where=news&query={quote(query)}"
+
+# 브라우저처럼 보이게 User-Agent 설정
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Referer": referer,
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "X-Requested-With": "XMLHttpRequest"
+    }
+
+# 쿠키: 필수 가능성이 높은 것만 사용
+cookies = {
+    "NNB": "JRCSWKUDHQJGG",
+    "page_uid": "jfagrwqo15Vss4l/afsssssssow-138069"
+}
+
+# Session 생성 (쿠키 자동 관리)
+session = requests.Session()
+
+# GET 요청 보내기
+response = session.get(url, params=params, headers=headers, cookies=cookies)
+
+print("상태코드", response.status_code)
+print("응답길이:",len(response.text))
+
+try:
+    # JSON 데이터로 파싱
+    data = response.json()
+
+    # HTML fragment 추출
+    html_fragment = data["collection"][0]["html"]
+
+    # BeautifulSoup HTML 파싱 객체 생성
+    soup = BeautifulSoup(html_fragment, "html.parser")
+
+    print(soup.prettify()[:1000])
+except ValueError:
+    print("JSON 파싱 실패, 응답 내용:", response.text[:500])
