aa4ea2ea930b1da36e3a9782f82c27ed1e7ad271
Trillion_Impulse <tkdduq2897@naver.com>
2025-12-04 17:57:48 +0900

    refactor(config.py): 각 practice 모듈의 URL 정보를 .env에서 불러오도록 수정

--- config.py
+++ config.py
@@ -12,3 +12,23 @@ DB_CONFIG = {
     "database": os.getenv("DB_NAME"),
     "charset": os.getenv("DB_CHARSET", "utf8mb4")
 }
+
+PRACTICE_CONFIGS={
+    "PRACTICE1" : {
+        "URL": os.getenv("PRACTICE1_URL")
+    },
+    "PRACTICE2" : {
+        "URL_BASE": os.getenv("PRACTICE2_URL_BASE")
+    },
+    "PRACTICE3" : {
+        "URL": os.getenv("PRACTICE3_URL")
+    },
+    "PRACTICE4" : {
+        "URL": os.getenv("PRACTICE4_URL"),
+        "QUERY": os.getenv("PRACTICE4_QUERY"),
+        "REFERER_BASE": os.getenv("PRACTICE4_REFERER_BASE")
+    },
+    "PRACTICE5" : {
+        "URL_BASE": os.getenv("PRACTICE5_URL_BASE")
+    }
+}
